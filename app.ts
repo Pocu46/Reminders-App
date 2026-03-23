760f5722f53ae2319e9291154cb7c268e7fdadf7
pocu46 <andrew.pulyaev@gmail.com>
2026-03-23 21:27:37 +0200

    Images routes are added to app.ts

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import remindersRoutes from './routes/reminders';
 import userRoutes from './routes/users';
 import path from 'path';
+import multer from 'multer';
 import {connectToDB} from "./utils/dbConnect";
 
 dotenv.config()
@@ -11,7 +12,31 @@ const app: Application = express()
 const PORT = process.env.PORT || 5000
 const API_PREFIX = process.env.API_PREFIX || '/api/v1'
 
+const fileStorage = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, callback: (error: Error | null, destination: string) => void) => {
+        callback(null, 'images')
+    },
+    filename: (req: Request, file: Express.Multer.File, callback: (error: Error | null, filename: string) => void) => {
+        callback(null, `${new Date().toISOString()}-${file.originalname.replace(/:/g, '-')}`)
+    }
+})
+
+const fileFilter = (req: Request, file: Express.Multer.File, callback: multer.FileFilterCallback) => {
+    if(file.mimetype === "image/png" || file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
+        callback(null, true)
+    } else {
+        callback(new Error('Invalid file type. Only PNG, JPEG and JPG are allowed.'))
+    }
+}
+
+// const upload = multer({
+//     storage: fileStorage,
+//     fileFilter,
+//     limits: { fileSize: 1024 * 1024 * 5 }
+// })
+
 app.use(express.json())
+app.use(multer({storage: fileStorage, fileFilter}).single('image'))
 app.use(express.urlencoded({ extended: true }))
 app.use(`${API_PREFIX}/images`, express.static(path.join(__dirname, 'images')))
 
@@ -19,6 +44,11 @@ app.use((req: Request, res: Response, next: NextFunction) =>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
     next()
 })
 
@@ -26,7 +56,7 @@ app.use(`${API_PREFIX}/user`, userRoutes)
 app.use(`${API_PREFIX}/reminders`, remindersRoutes)
 
 app.use((error: Error & {statusCode?: number}, req: Request, res: Response, next: NextFunction) =>{
-    console.log(error)
+    console.error(error)
     const status = error.statusCode || 500
     const message = error.message
     res.status(status).send({success: false, message})
